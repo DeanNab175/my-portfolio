d088a7f19b53db9cd8cfa90aab3489e56811cf7d
deannab175 <f.jscript@gmail.com>
2024-09-29 15:59:43 +0400

    add navbar component and favicon and some pages

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,9 +1,42 @@
+import Link from "next/link";
 import styles from "./navbar.module.scss";
+
+const navLinks = [
+  {
+    id: "home",
+    url: "/",
+    label: "Home",
+  },
+  {
+    id: "about",
+    url: "/about",
+    label: "About",
+  },
+  {
+    id: "skills",
+    url: "/skills",
+    label: "Skills",
+  },
+  {
+    id: "portfolio",
+    url: "/portfolio",
+    label: "Portfolio",
+  },
+  {
+    id: "contact",
+    url: "/contact",
+    label: "Contact",
+  },
+];
 
 function Navbar() {
   return (
     <div className={styles.navbar}>
-      <h1>Navbar</h1>
+      {navLinks.map((nav) => (
+        <Link key={nav.id} href={nav.url}>
+          {nav.label}
+        </Link>
+      ))}
     </div>
   );
 }
